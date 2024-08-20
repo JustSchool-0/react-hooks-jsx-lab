e9c4be1771648db0e042f29a99678bdd08c66049
Tori <7orivorian+github@gmail.com>
2024-08-20 16:01:49 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,13 @@ import Home from "./Home";
 import About from "./About";
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+    return (
+        <div>
+            <NavBar/>
+            <Home/>
+            <About/>
+        </div>
+    );
 }
 
 export default App;
